5c23eafcfe080352fc9fcd22bb78e881ea25865c
Mark Kelsall <99473523+geekissh@users.noreply.github.com>
2023-12-14 05:43:55 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// Code your solution in this file!
+// index.js
+
+// Declare and initialize companyName
+const companyName = 'Scuber';
+
+// Declare mostProfitableNeighborhood as a let and initialize it
+let mostProfitableNeighborhood = 'Chelsea';
+
+// Declare companyCeo as a let and initialize it
+let companyCeo = 'Susan Smith';
+ 
+// run test and pray it works
